68d4f3d1afc9f99393d612159316a44ae03f24a0
SANNY-GIT <kimlofia69@gmail.com>
2024-06-09 23:37:54 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -53,7 +53,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send("⛔️ Access Denied 🥸")
   }
   }
-        if(event.reaction == "🤬"){
+        if(event.reaction == "😡"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100057678948022"){
     message.unsend(event.messageID)
 }else{
