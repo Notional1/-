bf4ba5a22e67a690fb7bf5f9fa5b9d48e47b7bd2
SANNY-GIT <kimlofia69@gmail.com>
2024-06-10 22:05:37 +0600

    Update art.js

--- scripts/cmds/art.js
+++ scripts/cmds/art.js
@@ -1,42 +1,104 @@
-const axios = require("axios");
+const axios = require('axios');
+const fs = require('fs');
 
-module.exports = {
-  config: {
-    name: "art",
-    role: 0,
-    author: "OtinXSandip",
-    countDown: 0,
-    longDescription: "Art images",
-    category: "AI",
-    guide: {
-      en: "${pn} reply to an image with a prompt and choose model 1 - 10"
-    }
+module.exports.config = {
+  name: "art", 
+  version: "1.0.0", 
+  role: 0,
+  author: "𝖬𝖱.𝖲𝖠𝖭𝖭𝖸",//**api dipto**//
+  description: "𝗣𝗿𝗼𝗺𝗽𝘁 𝘁𝗼 𝗽𝗵𝗼𝘁𝗼, 𝗽𝗵𝗼𝘁𝗼 𝘁𝗼 𝗽𝗵𝗼𝘁𝗼",
+  category: "𝗜𝗠𝗔𝗚𝗘", 
+  guide: { 
+    en:"prompt | reply a photo"
   },
-  onStart: async function ({ message, api, args, event }) {
-    const text = args.join(' ');
-    
-    if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
-      return message.reply("reply to image");
+  countDown: 10
+};
+
+module.exports.onReply = async function ({ api, event , args}) {
+  if (event.type == "message_reply") {
+    let mod = args[0] || "1";
+    let prompt = args.slice(1).join(" ").toLowerCase() || "anime type";
+    const url = event.messageReply.attachments[0].url;
+    if (isNaN(url)) {
+      try {
+        api.setMessageReaction("💋", event.messageID, (err) => {}, true);
+        const response = await axios.get(`https://noobs-api2.onrender.com/dipto/genix?url=${encodeURIComponent(url)}&prompt=${encodeURIComponent(prompt)}&model=${mod}`);
+        const data = response.data.data;
+        await api.sendMessage({ 
+          body: "✅ | 𝐇𝐀𝐑𝐄 𝐘𝐎𝐔𝐑 𝐀𝐑𝐓 𝐏𝐇𝐎𝐓𝐎", 
+          attachment: await global.utils.getStreamFromURL(data)
+        }, event.threadID, (error, info) => {
+global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            type: 'reply',
+            messageID: info.messageID,
+            author: event.senderID,
+            link: data
+          });
+        }, event.messageID);
+      } catch (error) {
+        console.log(error);
+      }
     }
+  }
+};
 
-    const imgurl = encodeURIComponent(event.messageReply.attachments[0].url);
-
-    const [model] = text.split('|').map((text) => text.trim());
-    const puti = model || "6";
-        
-    api.setMessageReaction("⏰", event.messageID, () => {}, true);
-    const lado = `https://sandipbaruwal.onrender.com/art?url=${imgurl}&model=${puti}`;
-
-   const baby = await require('tinyurl').shorten(lado);
-
-message.reply("✅| Generating please wait.", async (err, info) => {
-      const attachment = await global.utils.getStreamFromURL(lado);
-      message.reply({  body: `${baby}`,
-        attachment: attachment
+module.exports.onStart = async function ({ api, args, event }) {
+  try {
+    let mod = args[0] || "1";
+    let prompt = args.slice(1).join(" ").toLowerCase() || "anime type";
+    if (event.type === "message_reply") {
+      const url = event.messageReply.attachments[0].url;
+const wait = api.sendMessage("𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓 𝐀𝐑𝐓 𝐋𝐎𝐀𝐃𝐈𝐍𝐆•••", event.threadID);
+      try {
+        const response = await axios.get(`https://noobs-api2.onrender.com/dipto/genix?url=${encodeURIComponent(url)}&prompt=${prompt}&model=${mod}`);
+        const link = response.data.data;
+        await api.sendMessage({ 
+          body: "✅ | 𝐇𝐀𝐑𝐄 𝐘𝐎𝐔𝐑 𝐀𝐑𝐓 𝐏𝐇𝐎𝐓𝐎", 
+          attachment: await global.utils.getStreamFromURL(link)
+        }, event.threadID, (error, info) => {
+global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            type: 'reply',
+            messageID: info.messageID,
+            author: event.senderID,
+            link: link
+          });
+        }, event.messageID);
+        api.unsendMessage(wait.messageID);
+      } catch (e) {
+        console.log(e);
+      }
+    } else if (prompt) {
+      const prom = args.join(" ")
+      const ratio = prom.split("--ratio")[1].trim() || "1:1"
+     // const weight = prom.split("--weight")[1].trim() || 0.8;
+      const wait = api.sendMessage("𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝐀𝐈𝐓 𝐀𝐑𝐓 𝐋𝐎𝐀𝐃𝐈𝐍𝐆•••", event.threadID);
+      const response = await axios.get(`https://noobs-api2.onrender.com/dipto/genix?prompt=${encodeURIComponent(prom)}&ratio=${ratio}`);
+      const link = response.data.data;
+      const filePath = __dirname + `/cache/genix.png`;
+      const respo = await axios.get(link, { responseType: 'stream' });
+      const writer = fs.createWriteStream(filePath);
+      respo.data.pipe(writer);
+      writer.on('finish', async () => {
+        await api.sendMessage({ 
+          body: "✅ | 𝐇𝐀𝐑𝐄 𝐘𝐎𝐔𝐑 𝐀𝐑𝐓 𝐏𝐇𝐎𝐓𝐎", 
+          attachment: fs.createReadStream(filePath)
+        }, event.threadID, (error, info) => {
+global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            type: 'reply',
+            messageID: info.messageID,
+            author: event.senderID,
+            link: link
+          });
+        }, event.messageID);
+        api.unsendMessage(wait.messageID);
+        fs.unlinkSync(filePath);
       });
-      let ui = info.messageID;          
-      message.unsend(ui);
-      api.setMessageReaction("✅", event.messageID, () => {}, true);
-    });
+    }
+  } catch (error) {
+    console.error(`Failed to generate: ${error}`);
+    api.sendMessage(`⚠️ | END: ${error.message}`, event.threadID, event.messageID);
   }
 };
